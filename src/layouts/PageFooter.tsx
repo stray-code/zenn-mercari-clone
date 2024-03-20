ea45794cdd5e958e0e5da6e95dfc523a1167dc4e
stray-code <abc1007020@gmail.com>
2024-03-20 19:45:23 +0900

    SNSのリンクアイコンとコピーライトを作成

--- src/layouts/PageFooter.tsx
+++ src/layouts/PageFooter.tsx
@@ -1,3 +1,5 @@
+import { Bird, Book } from 'lucide-react';
+
 export const PageFooter = () => {
   return (
     <footer className="bg-neutral-100">
@@ -6,7 +8,19 @@ export const PageFooter = () => {
         <div className="hidden gap-5 px-10 lg:flex">PCサイズのメニュー</div>
       </div>
       <div className="border-t border-secondary-border px-8 py-6">
-        SNSのリンクアイコンとコピーライト
+        <div className="flex flex-col justify-between gap-5 lg:flex-row lg:items-center">
+          <div className="flex items-center gap-5">
+            <a href="/">
+              <Bird className="size-[32px] stroke-2" />
+            </a>
+            <a href="/">
+              <Book className="size-[32px] stroke-2" />
+            </a>
+          </div>
+          <span className="text-xs text-secondary-text">
+            &copy; Caricari, Inc.
+          </span>
+        </div>
       </div>
     </footer>
   );
