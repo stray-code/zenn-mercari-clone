3cdf15c61869f92eadbc7afee1086d16b5255a2c
stray-code <abc1007020@gmail.com>
2024-03-20 18:22:58 +0900

    tailwindのカラーパレット設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import colors from "tailwindcss/colors"
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -5,7 +7,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        secondary: {
+          hover: colors.gray[100],
+          border: colors.gray[300],
+          text: colors.gray[600],
+        },
+      },
+    },
   },
   plugins: [],
 }
