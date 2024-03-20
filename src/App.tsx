cd292a26bd3ae5decb9f64417b35e031bd485580
stray-code <abc1007020@gmail.com>
2024-03-20 18:30:17 +0900

    ページナビゲーションを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { navigationMenus } from './constants';
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <div className="min-h-screen"></div>
+      <nav className="sticky bottom-0 z-10 border-t border-secondary-border bg-white lg:hidden">
+        <ul className="grid grid-cols-4">
+          {navigationMenus.map((menu, index) => (
+            <li key={index}>
+              <a
+                href={menu.link}
+                className="flex flex-col items-center justify-center gap-1 py-1.5 text-xs hover:bg-secondary-hover"
+              >
+                <menu.icon className="size-[24px] stroke-2" />
+                {menu.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
   );
 }
-
-export default App;
---
+++ src/constants/navigationMenus.ts
@@ -0,0 +1,24 @@
+import { Bell, Home, Camera, User } from 'lucide-react';
+
+export const navigationMenus = [
+  {
+    link: '/',
+    label: 'ホーム',
+    icon: Home,
+  },
+  {
+    link: '/',
+    label: 'お知らせ',
+    icon: Bell,
+  },
+  {
+    link: '/',
+    label: '出品',
+    icon: Camera,
+  },
+  {
+    link: '/',
+    label: 'マイページ',
+    icon: User,
+  },
+];
