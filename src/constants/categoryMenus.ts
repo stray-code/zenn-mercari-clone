3c3be38cbef42246d323cf148840376bd4da6024
stray-code <abc1007020@gmail.com>
2024-03-20 18:45:52 +0900

    カテゴリの定数を作成

---
+++ src/constants/categoryMenus.ts
@@ -0,0 +1,36 @@
+import { Menu } from '../types';
+
+export const categoryMenus: Menu[] = [
+  {
+    link: '/',
+    label: 'おすすめ',
+  },
+  {
+    link: '/',
+    label: 'マイリスト',
+  },
+  {
+    link: '/',
+    label: 'ゲーム・おもちゃ・グッズ',
+  },
+  {
+    link: '/',
+    label: '本・雑誌・漫画',
+  },
+  {
+    link: '/',
+    label: 'メンズ',
+  },
+  {
+    link: '/',
+    label: 'レディース',
+  },
+  {
+    link: '/',
+    label: 'ベビー・キッズ',
+  },
+  {
+    link: '/',
+    label: 'すべて見る',
+  },
+];
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,2 @@
 export * from './navigationMenus';
+export * from './categoryMenus';
